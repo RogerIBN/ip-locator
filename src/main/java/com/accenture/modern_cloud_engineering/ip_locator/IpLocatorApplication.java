747c5d20b8eb929c398038224fb762ef984f1017
Roger Barrera <rogaba_2@hotmail.com>
2022-12-10 01:50:29 -0600

    first commit

---
+++ src/main/java/com/accenture/modern_cloud_engineering/ip_locator/IpLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.modern_cloud_engineering.ip_locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/modern_cloud_engineering/ip_locator/IpLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.modern_cloud_engineering.ip_locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
