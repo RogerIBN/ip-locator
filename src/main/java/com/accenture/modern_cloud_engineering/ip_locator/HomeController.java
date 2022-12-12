78a88ce056b8ecf9084e5390005cfe46f9702a5b
Roger Barrera <rogaba_2@hotmail.com>
2022-12-12 04:23:50 -0600

    Estructura básica de la app

--- src/main/java/com/accenture/modern_cloud_engineering/ip_locator/HomeController.java
+++ src/main/java/com/accenture/modern_cloud_engineering/ip_locator/HomeController.java
@@ -2,6 +2,7 @@ package com.accenture.modern_cloud_engineering.ip_locator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,22 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/example")
+    public Object example() {
+        String url = "https://api.ipify.org/?format=json";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Object result = restTemplate.getForObject(url, Object.class);
+
+        return result;
+
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
     }
 }
