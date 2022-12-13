e88e6d41d3f2ff1b5a2e28676dd2506847ceeccf
Roger Barrera <rogaba_2@hotmail.com>
2022-12-13 01:30:46 -0600

    move home controller

---
+++ src/main/java/com/accenture/modern_cloud_engineering/ip_locator/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.accenture.modern_cloud_engineering.ip_locator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
