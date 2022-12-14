58083b66427174b5ec4a7dc3ff3264da7d4b1dc1
Roger Barrera <rogaba_2@hotmail.com>
2022-12-14 04:10:14 -0600

    comment code

--- src/main/resources/static/js/code.js
+++ src/main/resources/static/js/code.js
@@ -1,7 +1,14 @@
 // Query the map of the user's IP address and display it in the browser
 $("#search-location").click(function () {
-  console.log("click");
-  $.get("api/v1/position", function (data) {
-    document.getElementById("mapa-ip").src = data.url;
+  // Log the click event
+  console.log(
+    "Click on the button to search for the location of the IP address"
+  );
+  // Get the data from the api
+  $.get("api/v1/position", function (openStreetMap) {
+    // Get the map element
+    let ipMap = document.getElementById("mapa-ip");
+    // Set the src attribute of the map element
+    ipMap.src = openStreetMap.url;
   });
 });
