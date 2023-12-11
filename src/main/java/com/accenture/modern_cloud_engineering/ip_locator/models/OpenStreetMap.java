6f355e0c79da5fdfd35815f5174b92e2518da747
Roger Barrera <70298206+RogerIBN@users.noreply.github.com>
2023-12-11 02:06:24 -0600

    refactor: :recycle: Refactor OpenStreetMap constructor and createUrl method
    This commit refactors the OpenStreetMap class by modifying the constructors and the createUrl method. The constructors now accept different parameters and utilize the createUrl method to set the URL. The createUrl method has also been modified to use a formatted string for constructing the URL. This improves code readability and maintainability.

--- src/main/java/com/accenture/modern_cloud_engineering/ip_locator/models/OpenStreetMap.java
+++ src/main/java/com/accenture/modern_cloud_engineering/ip_locator/models/OpenStreetMap.java
@@ -3,22 +3,19 @@ package com.accenture.modern_cloud_engineering.ip_locator.models;
 public class OpenStreetMap {
     private String url;
 
-    public OpenStreetMap(IpInfo ipInfo) {
-        String[] location = ipInfo.getLoc().split(",");
-        String url = createUrl(location);
-        this.url = url;
-    }
-
-    public OpenStreetMap(String[] location) {
-        String url = createUrl(location);
-        this.url = url;
+    public OpenStreetMap() {
     }
 
     public OpenStreetMap(String url) {
         this.url = url;
     }
 
-    public OpenStreetMap() {
+    public OpenStreetMap(String[] location) {
+        this(createUrl(location));
+    }
+
+    public OpenStreetMap(IpInfo ipInfo) {
+        this(ipInfo.getLoc().split(","));
     }
 
     /**
@@ -27,25 +24,24 @@ public class OpenStreetMap {
      * @param location The latitude and longitude
      * @return The URL
      */
-    private String createUrl(String[] location) {
+    private static String createUrl(String[] location) {
         // Split the position string into latitude and longitude
         String latitude = location[0];
         String longitude = location[1];
 
         // Fill the latitude and longitude in the URL with 0's on the right to fill 18
         // characters
-        latitude = String.format("%-18s", latitude).replace(' ', '0');
-        longitude = String.format("%-18s", longitude).replace(' ', '0');
+        latitude = "%-18s".formatted(latitude).replace(' ', '0');
+        longitude = "%-18s".formatted(longitude).replace(' ', '0');
         // Link examples:
         // "https://www.openstreetmap.org/export/embed.html?bbox=-89.67229843139648%2C20.938113990727835%2C-89.57651138305665%2C20.989409533505412&amp;layer=mapnik&amp;marker=20.963763961508004%2C-89.62440490722656"
         // "https://www.openstreetmap.org/export/embed.html?bbox=-89.62066590785982%2C20.973797313831863%2C-89.61467921733858%2C20.977003035818043&amp;layer=mapnik&amp;marker=20.97540018342022%2C-89.61767256259918"
         // BUG: The map doesn't show the marker
 
         // Create the URL
-        return "https://www.openstreetmap.org/export/embed.html?bbox=" +
-                longitude + "%2C" + latitude + "%2C"
-                + longitude + "%2C" + latitude + "&amp;layer=mapnik&amp;marker=" + latitude +
-                "%2C" + longitude;
+
+        String urlFormat = "https://www.openstreetmap.org/export/embed.html?bbox=%2$s%%2C%1$s%%2C%2$s%%2C%1$s&amp;layer=mapnik&amp;marker=%1$s%%2C%2$s";
+        return String.format(urlFormat, latitude, longitude);
     }
 
     public String getUrl() {
